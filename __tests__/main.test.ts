b4f841ff4d2b148fdd2b45608ee8d724273d2385
Thomas Carmet <thomas.carmet@scality.com>
2021-12-17 16:11:48 -0800

    setup upload artifacts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -21,3 +21,24 @@ test('test runs setup', () => {
   }
   console.log(cp.execFileSync(np, [ip], options).toString())
 })
+
+test('test runs upload', () => {
+  process.env['INPUT_METHOD'] = 'upload'
+  process.env['INPUT_SOURCE'] = '/tmp/'
+  // process.env['INPUT_URL'] = 'http://artifacts'
+  // process.env['INPUT_USER'] = 'username-pass'
+  // process.env['INPUT_PASSWORD'] = 'pass'
+  process.env['GITHUB_REPOSITORY'] = 'owner/repo'
+  process.env['GITHUB_WORKFLOW'] = 'worklow name'
+  process.env['GITHUB_SHA'] = '650d9185ff9ad990de710b078a0579eb2bd64bd8'
+  process.env['GITHUB_RUN_NUMBER'] = '42'
+  process.env['GITHUB_RUN_ATTEMPT'] = '1'
+  process.env['GITHUB_EVENT_NAME'] = 'pull_request'
+
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+})
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,57 @@
 import * as core from '@actions/core'
+import * as glob from '@actions/glob'
+import fs from 'fs'
+import FormData from 'form-data'
+import axios from 'axios'
+import {AxiosRequestConfig} from 'axios'
 import {artifactsName, setOutputs} from './artifacts'
 
 async function setup(): Promise<void> {
   const name: string = await artifactsName()
 
+  await setOutputs(name)
+}
+
+async function upload(): Promise<void> {
+  const source: string = core.getInput('source')
+  const name: string = await artifactsName()
+  const user: string = core.getInput('user')
+  const url: string = core.getInput('url')
+  const password: string = core.getInput('password')
+
+  const globber = await glob.create(source)
+  for await (const file of globber.globGenerator()) {
+    const stat: fs.Stats = await fs.statSync(file)
+
+    if (stat.isFile()) {
+      const form: FormData = new FormData()
+      const request_config: AxiosRequestConfig = {
+        headers: {
+          ...form.getHeaders()
+        },
+        auth: {
+          username: user,
+          password: password
+        },
+        maxBodyLength: Infinity,
+        maxContentLength: Infinity
+      }
+
+      form.append('file', fs.createReadStream(file))
+      core.info(`Uploading file: ${file}`)
+      axios.put(
+        `${url}/upload/${name}/${file.replace(source, '')}`,
+        form,
+        request_config
+      )
+      .then((response) => {
+        core.info(`${file} has been uploaded`)
+      })
+      .catch((error) => {
+          throw error
+      })
+    }
+  }
   await setOutputs(name)
 }
 
@@ -15,6 +63,8 @@ async function run(): Promise<void> {
 
     if (method === 'setup') {
       await setup()
+    } else if (method === 'upload') {
+      await upload()
     } else {
       throw new Error(`Method ${method} does not exist`)
     }
