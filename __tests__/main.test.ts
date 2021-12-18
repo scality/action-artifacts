4d4f0fb253b4153c24fa655bd75b2daaa9bdf843
Thomas Carmet <thomas.carmet@scality.com>
2021-12-17 16:11:48 -0800

    setup upload artifacts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,18 +1,54 @@
 import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
-import {test} from '@jest/globals'
+import {afterEach, beforeEach, test} from '@jest/globals'
+import fs from 'fs'
+import os from 'os'
+
+beforeEach(() => {
+  process.env['GITHUB_REPOSITORY'] = 'owner/repo'
+  process.env['GITHUB_WORKFLOW'] = 'worklow name'
+  process.env['GITHUB_SHA'] = '650d9185ff9ad990de710b078a0579eb2bd64bd8'
+  process.env['GITHUB_RUN_NUMBER'] = Math.floor(Math.random() * 100).toString()
+  process.env['GITHUB_RUN_ATTEMPT'] = '1'
+
+  if (process.env['INPUT_URL'] == undefined) {
+    process.env['INPUT_URL'] = 'http://artifacts'
+  }
+  const tmpDir: string = fs.mkdtempSync(
+    path.join(os.tmpdir(), 'test-action-artifacts')
+  )
+  process.env['GITHUB_WORKSPACE'] = tmpDir
+})
+
+afterEach(() => {
+  const workspace: string = process.env['GITHUB_WORKSPACE'] || ''
+  if (workspace) {
+    fs.rmSync(workspace, {recursive: true})
+  }
+})
 
 // shows how the runner will run a javascript action with env / stdout protocol
 test('test runs setup', () => {
   process.env['INPUT_METHOD'] = 'setup'
-  process.env['INPUT_URL'] = 'http://artifacts'
-  process.env['GITHUB_REPOSITORY'] = 'owner/repo'
-  process.env['GITHUB_WORKFLOW'] = 'worklow name'
-  process.env['GITHUB_SHA'] = '650d9185ff9ad990de710b078a0579eb2bd64bd8'
-  process.env['GITHUB_RUN_NUMBER'] = '42'
-  process.env['GITHUB_RUN_ATTEMPT'] = '1'
-  process.env['GITHUB_EVENT_NAME'] = 'pull_request'
+
+  const np = process.execPath
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecFileSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execFileSync(np, [ip], options).toString())
+})
+
+test('test runs upload', () => {
+  const workspace: string = process.env['GITHUB_WORKSPACE'] || ''
+  const source: string = 'artifacts'
+  process.env['INPUT_METHOD'] = 'upload'
+
+  fs.mkdirSync(path.join(workspace, source))
+  fs.writeFileSync(path.join(workspace, source, 'file.txt'), 'content')
+
+  process.env['INPUT_SOURCE'] = source
 
   const np = process.execPath
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
--- src/artifacts.ts
+++ src/artifacts.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import * as process from 'process'
+import axios, {AxiosRequestConfig, AxiosResponse} from 'axios'
+import fs from 'fs'
 
 export async function artifactsName(): Promise<string> {
   const owner: string = github.context.repo.owner
@@ -16,9 +18,32 @@ export async function artifactsName(): Promise<string> {
   return `github:${owner}:${repo}:staging-${commit}.${workflow}.${runNumber}.${runAttempt}`
 }
 
-export async function setOutputs(name: string): Promise<void> {
-  const url: string = core.getInput('url')
-
+export async function setOutputs(name: string, url: string): Promise<void> {
   core.setOutput('name', name)
   core.setOutput('link', `${url}/builds/${name}`)
+}
+
+export async function setNotice(name: string, url: string): Promise<void> {
+  core.info(
+    `::notice:: Your artifacts has been uploaded here: ${url}/builds/${name}`
+  )
+}
+
+export async function fileUpload(
+  url: string,
+  username: string,
+  password: string,
+  file: string
+): Promise<AxiosResponse> {
+  const fileStream: fs.ReadStream = fs.createReadStream(file)
+  const request_config: AxiosRequestConfig = {
+    auth: {
+      username,
+      password
+    },
+    maxBodyLength: Infinity,
+    maxContentLength: Infinity
+  }
+
+  return await axios.put(url, fileStream, request_config)
 }
