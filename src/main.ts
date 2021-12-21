f1b7076a5756df3d3f80dbdd736f1f45da216d36
Thomas Carmet <thomas.carmet@scality.com>
2021-12-21 09:49:04 -0800

    [RELENG-5274] Add Promote

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as exec from '@actions/exec'
 import * as glob from '@actions/glob'
 import * as path from 'path'
 import * as process from 'process'
@@ -11,6 +12,76 @@ async function setup(): Promise<void> {
   const url: string = core.getInput('url')
 
   await setOutputs(name, url)
+}
+
+async function promote(): Promise<void> {
+  let myOutput = ''
+  const options: exec.ExecOptions = {}
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      myOutput += data.toString()
+    }
+  }
+
+  const user: string = core.getInput('user')
+  const password: string = core.getInput('password')
+  const url: string = core.getInput('url')
+  const name: string = core.getInput('name')
+  const tag: string = core.getInput('tag')
+
+  const staging_regex = new RegExp(
+    '(^[^/]+:)(staging|prolonged)-([0-9a-f]+).[^./]+.[0-9]+.[0-9]+$'
+  )
+  const promoted_regex = new RegExp('(^[^/]+:)promoted-([^/]+)$')
+
+  const staging_match = name.match(staging_regex)
+  const promoted_match = name.match(promoted_regex)
+
+  let artifacts_commit = ''
+  let artifacts_prefix = ''
+  if (staging_match !== null) {
+    artifacts_commit = staging_match[3]
+    artifacts_prefix = staging_match[1]
+  } else if (promoted_match !== null) {
+    const artifacts_tag = promoted_match[2]
+    myOutput = ''
+    await exec.exec('git', ['rev-list', '-n', '1', artifacts_tag], options)
+    artifacts_commit = myOutput
+    artifacts_prefix = promoted_match[1]
+  } else {
+    throw Error(
+      "This artifacts doesn't match Scality's artifacts naming standards"
+    )
+  }
+
+  myOutput = ''
+  await exec.exec('git', ['rev-list', '-n', '1', tag], options)
+  if (!myOutput.includes(artifacts_commit))
+    throw Error(
+      `Tag commit ${artifacts_commit} don't match the artifacts commit ${myOutput}`
+    )
+
+  const promoted_name = `${artifacts_prefix}promoted-${tag}`
+  const final_url: string = new URL(
+    path.join('/copy/', name, promoted_name),
+    url
+  )
+    .toString()
+    .concat('/')
+  const request_config: AxiosRequestConfig = {
+    auth: {
+      username: user,
+      password
+    },
+    maxBodyLength: Infinity,
+    maxContentLength: Infinity
+  }
+  const response = await axios.get(final_url, request_config)
+  if (response.status !== 200 || !response.data.includes('BUILD COPIED')) {
+    throw Error(`Build not copied, ${response.status}: ${response.data}`)
+  }
+  await setOutputs(promoted_name, url)
+  await setNotice(promoted_name, url)
 }
 
 async function prolong(): Promise<void> {
@@ -111,6 +182,8 @@ async function run(): Promise<void> {
       await upload()
     } else if (method === 'prolong') {
       await prolong()
+    } else if (method === 'promote') {
+      await promote()
     } else {
       throw new Error(`Method ${method} does not exist`)
     }
