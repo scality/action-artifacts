01f752571ed682aacb49233c1b4e5653152ab463
lavigne958 <alexandre.lavigne@scality.com>
2022-10-25 18:07:27 +0200

    RELENG-6655 - stop using `save-state`
    Use the new environment file to save the state of the action.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
-import * as coreCommand from '@actions/core/lib/command'
+import * as fs from 'fs'
+import * as os from 'os'
 import {InputsArtifacts} from './inputs-helper'
 import {getInputs} from './inputs-artifacts'
 import {setDefaultIndex} from './artifacts'
@@ -29,7 +30,12 @@ if (!IsPost) {
   run(inputs)
   // Publish a variable so that when the POST action runs, it can determine it should run the post logic.
   // This is necessary since we don't have a separate entry point
-  coreCommand.issueCommand('save-state', {name: 'isPost'}, 'true')
+  let GH_STATE_FILE = ''
+  if (process?.env?.GITHUB_STATE) {
+    //appendFileSync only accepts string, not 'undefined'.
+    GH_STATE_FILE = process.env.GITHUB_STATE
+  }
+  fs.appendFileSync(GH_STATE_FILE, `isPost=true${os.EOL}`, {encoding: 'utf8'})
 }
 
 // Post
