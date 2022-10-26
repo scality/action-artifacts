3fcb9d7467d1b8c10f8f04f465efbba4f064a230
lavigne958 <alexandre.lavigne@scality.com>
2022-10-26 14:51:43 +0200

    RELENG-6655 - use latests version of `save-state` from @action/core

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import * as core from '@actions/core'
-import * as fs from 'fs'
-import * as os from 'os'
 import {InputsArtifacts} from './inputs-helper'
 import {getInputs} from './inputs-artifacts'
 import {setDefaultIndex} from './artifacts'
@@ -30,12 +28,7 @@ if (!IsPost) {
   run(inputs)
   // Publish a variable so that when the POST action runs, it can determine it should run the post logic.
   // This is necessary since we don't have a separate entry point
-  let GH_STATE_FILE = ''
-  if (process?.env?.GITHUB_STATE) {
-    //appendFileSync only accepts string, not 'undefined'.
-    GH_STATE_FILE = process.env.GITHUB_STATE
-  }
-  fs.appendFileSync(GH_STATE_FILE, `isPost=true${os.EOL}`, {encoding: 'utf8'})
+  core.saveState('isPost', 'true')
 }
 
 // Post
